a641af46fe1009944aada8d5271dfa7886638b5b
shanmarkus <shanmarkus@gmail.com>
2014-05-13 19:36:53 +0700

    Adding Approve Promotion Fragment

---
+++ src/com/example/storeinbusiness/ApprovePromotionFragment.java
@@ -0,0 +1,112 @@
+package com.example.storeinbusiness;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+public class ApprovePromotionFragment extends Fragment {
+
+	protected final static String TAG = ApprovePromotionFragment.class.getSimpleName()
+			.toString();
+
+	// UI Variable
+	Button mDiscoverButtonCheckIn;
+	Button mDiscoverButtonBrowse;
+	Button mDiscoverButtonReccomendation;
+
+	// Variables
+	ArrayList<HashMap<String, String>> placesInfo = new ArrayList<HashMap<String, String>>();
+	protected ArrayList<String> placesID = new ArrayList<String>();
+	HashMap<String, String> placeInfo = new HashMap<String, String>();
+
+	// Parse Constants
+
+	public static ApprovePromotionFragment newInstance(String param1, String param2) {
+		ApprovePromotionFragment fragment = new ApprovePromotionFragment();
+		Bundle args = new Bundle();
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	public ApprovePromotionFragment() {
+		// Required empty public constructor
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		View view = inflater.inflate(R.layout.fragment_approve_promotion, container,
+				false);
+
+		// Declare UI
+		mDiscoverButtonBrowse = (Button) view
+				.findViewById(R.id.discoverButtonBrowse);
+		mDiscoverButtonCheckIn = (Button) view
+				.findViewById(R.id.discoverButtonCheckIn);
+		mDiscoverButtonReccomendation = (Button) view
+				.findViewById(R.id.discoverButtonDiscover);
+
+		return view;
+	}
+
+	/*
+	 * Function Added
+	 */
+
+	/*
+	 * Error Dialog
+	 */
+	private void errorAlertDialog(ParseException e) {
+		// failed
+		Log.e(TAG, e.getMessage());
+		AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+		builder.setMessage(e.getMessage()).setTitle("Error")
+				.setPositiveButton(android.R.string.ok, null);
+		AlertDialog dialog = builder.create();
+		dialog.show();
+	}
+
+}
