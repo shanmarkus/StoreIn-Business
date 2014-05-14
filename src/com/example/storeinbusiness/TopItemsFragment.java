41c07be69118fe5e946c75d1d1146a605cab789b
shanmarkus <shanmarkus@gmail.com>
2014-05-14 21:39:21 +0700

    Adding function

--- src/com/example/storeinbusiness/TopItemsFragment.java
+++ src/com/example/storeinbusiness/TopItemsFragment.java
@@ -5,27 +5,20 @@ import java.util.HashMap;
 import java.util.List;
 
 import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.SimpleAdapter;
 
 import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.ParseException;
-import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 
@@ -127,6 +120,38 @@ public class TopItemsFragment extends Fragment {
 			}
 		});
 	}
+
+	/*
+	 * Set Adapter
+	 */
+
+	private void setAdapter() {
+		// dismiss the progress dialog
+		String[] keys = { ParseConstants.KEY_NAME,
+				ParseConstants.KEY_TOTAL_LOVED };
+		int[] ids = { android.R.id.text1, android.R.id.text2 };
+
+		SimpleAdapter adapter = new SimpleAdapter(getActivity(), itemsInfo,
+				android.R.layout.simple_list_item_2, keys, ids);
+
+		// mListUsersReview.setAdapter(adapter);
+	}
+
+	/*
+	 * On Click Listener
+	 */
+
+	OnItemClickListener itemListener = new OnItemClickListener() {
+
+		@Override
+		public void onItemClick(AdapterView<?> parent, View view, int position,
+				long id) {
+			Intent intent = new Intent(getActivity(), ItemDetail.class);
+			intent.putExtra(ParseConstants.KEY_ITEM_ID, itemId.get(position));
+			startActivity(intent);
+
+		}
+	};
 
 	/*
 	 * Error Dialog
